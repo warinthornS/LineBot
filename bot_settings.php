64dcbec33eb2c29074b8b0d80e0ba25634df3bb7
warinthornS <boom_5912@hotmail.com>
2018-02-20 17:12:09 +0700

    Create bot_settings.php

---
+++ bot_settings.php
@@ -0,0 +1,6 @@
+<?php
+/// การตั้งค่าเกี่ยวกับ bot ใน LINE Messaging API
+define('LINE_MESSAGE_CHANNEL_ID','1564005574');
+define('LINE_MESSAGE_CHANNEL_SECRET','cda1740d524660beecc93ce95cb6895e');
+define('LINE_MESSAGE_ACCESS_TOKEN','qQAIscwhCNxVm/aKR+GC2fHKmuu2FLP9+F9R2iuqtZaCMKPaSNZVGca6ihSiytD8QvXTUnJDn7xk29xaesmos0x8XqAwIEgXHHPivMcTi0++nhLaIPgyaeknXRVi/wY52rQ1rTHGZDxYIi/uNBkCxAdB04t89/1O/w1cDnyilFU=');
+?>
